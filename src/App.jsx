126fe46b9ca92f66f90598ce0205551e47cb1c6b
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-22 23:07:27 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+    <div className='grid place-content-center h-screen'>
+      <h1 className='text-3xl text-bold'>Welcome to the background Removal</h1>
+    </div>
+    
+    
+    </>
+
+  )
+}
+
+export default App
